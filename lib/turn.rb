619719545a1a52fa62f4b84da0357ed53878c896
Natalia Galan <n8galan@gmail.com>
2020-06-13 19:56:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,47 +30,4 @@ def valid_move?(board, index)
    end
 end
 
-def turn_count(board)
-   counter = 0
-   board.each do |spaces|
-      if spaces == "X" || spaces == "O"
-         counter += 1
-      end
-   end
-   counter
-end
-def current_player(board)
-   turn_count(board) % 2 == 0 ? "X" : "O"
-end
 
-def turn(board)
-  puts "Please enter 1-9:"
-end
-
-def play(board)
-   until over?(board)
-      turn(board)
-   end
-   if won?(board)
-      winner(board) == "X" || winner(board) == "O"
-      puts "Congratulations #{winner(board)}!"
-   elsif draw?(board)
-      puts "Draw!"
-   end
-end
-
-def turn(board)
-   puts "Please enter 1-9:"
-   user_input = gets.strip
-   index = input_to_index(user_input)
-   if valid_move?(board, index)
-      move(board, index, current_player(board))
-      turn(board)
-   end
-   display_board(board)
-end
-
-
-def full?(board)
-   board.all? {|i| i == "X" || i == "O"}
-end
