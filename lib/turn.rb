85677a43b82d9a11637a414f0b91958abf755217
Natalia Galan <n8galan@gmail.com>
2020-06-13 20:35:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,8 +36,8 @@ def turn(board)
    index = input_to_index(user_input)
    if valid_move?(board, index)
       move(board, index, player = "X")
-      display(board)
+      turn(board)
    else
-   display_board(board)
+   turn(board)
 end
 end
