7d624a08ed3cc24c49a11aa9e2774367a1106c0c
Natalia Galan <n8galan@gmail.com>
2020-06-13 20:22:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
    user_input = gets.strip
    index = input_to_index(user_input)
    if valid_move?(board, index)
-      move(board, index, current_player(board))
+      move(board, index)
       turn(board)
    end
    display_board(board)
